7b526c265aa5dad0edb797be12e73d97464a6fe6
Gnanakumar01 <sgnanakumar929@gmail.com>
2026-02-15 14:03:44 +0530

    Skill section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import About from './components/About/About'
+import Skills from './components/Skills/Skills'
 import './App.css'
 import './components/About/About.css'
 import './components/Navbar/Navbar.css'
 import './components/Home/Home.css'
+import './components/Skills/Skills.css'
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
     <Navbar/>
     <Home/>
     <About/>
+    <Skills/>
     </>
   )
 }
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,65 @@
+function Skills() {
+    return (
+        <>
+            <section className="skills-container">
+                <div className="skill-heading">
+                    <h1>Explore My <span>Skills</span></h1>
+                    <p id="sub-head">Technologies that power my fronted developmebt toolkit</p>
+                </div>
+                <div className="tech-container">
+                    <div className="tech-sub-container">
+                        <div>
+                            <h3>FrontEnd Architecture & UI</h3>
+                            <p>Technologies for building type-safe, responsive, and high-perfomance user interfaces,</p>
+                        </div>
+                        <div className="tech-logos-container">
+                            <img src="/images/skills/html.png"
+                                className="tech-img" alt="hml" title="html" /> <span className="tooltip">Html</span>
+
+                            <img src="/images/skills/css-3.png" alt="css3"
+                                className="tech-img" title="css3" />
+
+                            <img src="/images/skills/bootstrap.png" alt="Bootstrap"
+                                className="tech-img" title="Bootstrap" />
+
+                            <img src="/images/skills/js.png" alt="JavaScript"
+                                className="tech-img" title="JavaScript" />
+
+                            <img src="/images/skills/react.svg" alt="React"
+                                className="tech-img" title="React" />
+                        </div>
+                    </div>
+
+                    <div className="tech-sub-container">
+                        <div>
+                            <h3>Python Full Stack & Security</h3>
+                            <p>Server-side security technologies for full-stack capability.</p>
+                        </div>
+
+                        <div className="tech-logos-container">
+                            <img src="/images/skills/python.png" alt="Python" className="tech-img" title="Python" />
+                            <img src="/images/skills/django.png" alt="Django" className="tech-img" title="Django" />
+                            <img src="/images/skills/database.png" alt="SQL" className="tech-img" title="SQL" />
+                        </div>
+                    </div>
+
+                    <div className="tech-sub-container">
+                        <div>
+                            <h3>Tools & Prefessional Worlflow</h3>
+                            <p>Complementry tools and platforms for codd quality and collaboration.</p>
+                        </div>
+
+                        <div className="tech-logos-container">
+                            <img src="/images/skills/git.svg" alt="Git" className="tech-img" title="Git" />
+                            <img src="/images/skills/github-original.svg" alt="GitHub" className="tech-img" title="GitHub" />
+                            <img src="/images/skills/mysql.png" alt="Mysql" className="tech-img" title="Mysql"/>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Skills 

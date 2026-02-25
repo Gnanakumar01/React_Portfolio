eb2a1ac59cfb68ab595bb4616ca0533ac5c165c0
Gnanakumar01 <sgnanakumar929@gmail.com>
2026-02-25 12:48:48 +0530

    contact-form styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './components/Home/Home'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
 import Project from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
 import './App.css'
 import './components/About/About.css'
 import './components/Navbar/Navbar.css'
 import './components/Home/Home.css'
 import './components/Skills/Skills.css'
 import './components/Projects/Projects.css'
+import './components/Contact/Contact.css'
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
     <About/>
     <Skills/>
     <Project/>
+    <Contact/>
     </>
   )
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,60 @@
+function Contact() {
+    return (
+        <>
+            <div className="contact-container">
+                <div className="heading">
+                    <h1>Get In Touch</h1>
+                    <p>Let’s connect—feel free to reach out for collaborations,
+                        opportunities, or just a friendly chat</p>
+                </div>
+
+                <div className="contact-info">
+                    <div className="input-container">
+                        <form>
+                            <h2>Send a Message</h2>
+                            <input type="text" placeholder="Your Name" />
+                            <input type="email" placeholder="Your Email" />
+                            <input type="text" placeholder="Subject" />
+                            <textarea placeholder="Your Message"></textarea>
+                            <button>Send Message <i class="bi bi-arrow-right"></i></button>
+                        </form>
+                    </div>
+
+                    <div className="address-container">
+                        <div className="">
+                            <div className="each-address">
+                                <div><i class="bi bi-envelope"></i></div>
+                                <div className="email-container">
+                                    <h4>EMAIL</h4>
+                                    <a href="">sgnanakumar929@gmail.com</a>
+                                </div>
+                            </div>
+
+                            <div className="each-address">
+                                <div><i class="bi bi-telephone"></i></div>
+                                <div className="phone-container">
+                                    <h4>PHONE</h4>
+                                    <a href="">+91-8098013644</a>
+                                </div>
+                            </div>
+
+                            <div className="each-address">
+                                <div><i class="bi bi-geo-alt"></i></div>
+                                <div className="email-container">
+                                    <h4>LOCATION</h4>
+                                    <p>BTM Layout, Bengaluru, Karnataka</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            this for map
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Contact
